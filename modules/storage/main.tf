79bb535c6dd2c1bfa4f0858f1b42c4ac613399e9
Johan Fredrik Wilvang <johan@wilvang.com>
2026-03-13 17:18:35 +0100

    feat(storage): create volumes based on a list of volume sizes
    Refs #4

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -4,13 +4,16 @@
 # Only runs if volume support is enabled and vms_with_volumes_ids is not empty
 # ---------------------------------------
 resource "openstack_blockstorage_volume_v3" "volume" {
-  for_each = length(var.vms_with_volumes_ids) > 0 ? {
-    for idx, vm_id in var.vms_with_volumes_ids :
-    idx => vm_id
+  for_each = length(var.volume_sizes) > 0 ? {
+    for idx, vol in var.volume_sizes : idx => vol
   } : {}
 
   name = "volume_${each.key}"
-  size = var.volume_size
+  size = each.value
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # ---------------------------------------
@@ -24,6 +27,10 @@ resource "openstack_compute_volume_attach_v2" "volume_attachment" {
     idx => vm_id
   } : {}
 
-  instance_id = var.vms_with_volumes_ids[each.key] # Use the key from for_each
+  instance_id = each.value
   volume_id   = openstack_blockstorage_volume_v3.volume[each.key].id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
